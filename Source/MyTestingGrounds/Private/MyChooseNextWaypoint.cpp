aba0bf2042dd925441944f142d8d9a9fae4c6c4a
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2018-06-16 12:34:17 +0200

    Making guard patrolling via. C++ (ChooseWaypoint-Function-Conversion)

--- Source/MyTestingGrounds/Private/MyChooseNextWaypoint.cpp
+++ Source/MyTestingGrounds/Private/MyChooseNextWaypoint.cpp
@@ -2,11 +2,26 @@
 
 #include "MyTestingGrounds/Public/MyChooseNextWaypoint.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "MyTestingGrounds/Public/MyPatrollingGuard.h"
 
 
 
 EBTNodeResult::Type UMyChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("BABYY!!!"));
+	// Get patrol points 
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	AMyPatrollingGuard* Guard = Cast<AMyPatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
+	TArray<AActor*> PatrolPoints = Guard->PatrolPointsCPP;
+	if(PatrolPoints.Num() < 1){ return EBTNodeResult::Succeeded; }
+
+	// Set next waypoint
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// Cycle index
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, (Index + 1) % PatrolPoints.Num());
+
 	return EBTNodeResult::Succeeded;
 }
--- Source/MyTestingGrounds/Public/MyChooseNextWaypoint.h
+++ Source/MyTestingGrounds/Public/MyChooseNextWaypoint.h
@@ -6,8 +6,6 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "MyChooseNextWaypoint.generated.h"
 
-class UBehaviorTreeComponent;
-
 /**
  * 
  */
@@ -16,6 +14,13 @@ class MYTESTINGGROUNDS_API UMyChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
+
 private:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
